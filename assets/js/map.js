bd8160f3e216860565b7655f5015b362c293c267
Jether Rodrigues <jetherrodrigues@gmail.com>
2020-03-26 23:26:33 -0300

    improved covid19 coverage map

--- assets/js/map.js
+++ assets/js/map.js
@@ -6,7 +6,7 @@ $( document ).ready(function() {
 });
 
 function initMap() {
-    map = L.map('covid19-coverage', {scrollWheelZoom:true}).setView([29.53523, 24.60938], 2);
+    map = L.map('covid19-coverage', {scrollWheelZoom:true}).setView([29.53523, 24.60938], 3);
 
     var openstreetmap = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
@@ -23,92 +23,93 @@ function initMap() {
     var baseLayers = {
         'Blank': empty,
         'OSM' : openstreetmap,
-        'OSM_Black' : openStreetMapBlackAndWhite
+        'OSM Black' : openStreetMapBlackAndWhite
     }
-    baseLayers.OSM_Black.addTo(map);    
+    baseLayers['OSM Black'].addTo(map);    
     
+    var overLayers = {};
+
     $.ajax({
-            type: 'GET',
-            url: rootEndpoint,
-            dataType: 'json',
-            async: false,
-            success: function(data) {
-                elements = data.data
-                elements.forEach(element => {
-                    let marker = {
-                            country: element.country,
-                            countryInfo: {
-                                lat: element.countryInfo.lat,
-                                long: element.countryInfo.long,
-                                flag: element.countryInfo.flag,
-                                iso3: element.countryInfo.iso3
-                            },
-                            cases: element.cases,
-                            todayCases: element.todayCases,
-                            deaths: element.deaths,
-                            todayDeaths: element.todayDeaths,
-                            recovered: element.recovered,
-                            active: element.active,
-                            critical: element.critical,
-                            casesPerOneMillion: element.casesPerOneMillion,
-                            deathsPerOneMillion: element.deathsPerOneMillion,
-                            countryCode: element.countryCode,
-                            confirmed: element.confirmed
-                        }
-                        
-                        var popupContent = 
-                            "<div class=\"table-responsive\">" + 
-                                "<table class=\"table table-striped table-sm\">" +
-                                    "<thead>" +
-                                        "<tr>" +                                   
-                                            "<th>Country</th>" + 
-                                            "<th>Cases</th>" + 
-                                            "<th>Cases Today</th>" + 
-                                            "<th>Deaths</th>" + 
-                                            "<th>Deaths Today</th>" + 
-                                            "<th>Recovered</th>" + 
-                                            "<th>Active</th>" + 
-                                            "<th>Confirmed</th>" + 
-                                        "</tr>" + 
-                                    "</thead>" + 
-                                    "<tbody>" + 
-                                        "<tr>" + 
-                                            "<td>" + marker.country + "</td>" + 
-                                            "<td>" + marker.cases + "</td>" + 
-                                            "<td>" + marker.todayCases + "</td>" + 
-                                            "<td>" + marker.deaths + "</td>" + 
-                                            "<td>" + marker.todayDeaths + "</td>" + 
-                                            "<td>" + marker.recovered + "</td>" + 
-                                            "<td>" + marker.active + "</td>" + 
-                                            "<td>" + marker.confirmed + "</td>" + 
-                                        "</tr>" + 
-                                    "</tbody>" + 
-                                "</table>" + 
-                            "</div>"
+        type: 'GET',
+        url: rootEndpoint,
+        dataType: 'json',
+        async: false,
+        success: function(data) {
+            let countries = new L.LayerGroup();
+            let heatRootPoints = new Array();
+            let elements = data.data
+            elements.forEach(element => {
+                let marker = {
+                        country: element.country,
+                        countryInfo: {
+                            lat: element.countryInfo.lat,
+                            long: element.countryInfo.long,
+                            flag: element.countryInfo.flag,
+                            iso3: element.countryInfo.iso3
+                        },
+                        cases: element.cases,
+                        todayCases: element.todayCases,
+                        deaths: element.deaths,
+                        todayDeaths: element.todayDeaths,
+                        recovered: element.recovered,
+                        active: element.active,
+                        critical: element.critical,
+                        casesPerOneMillion: element.casesPerOneMillion,
+                        deathsPerOneMillion: element.deathsPerOneMillion,
+                        countryCode: element.countryCode,
+                        confirmed: element.confirmed
+                    }
+                    
+                var popupContent = 
+                    "<div class=\"table-responsive\"><table class=\"table table-striped table-sm\">" +
+                            "<thead><tr><th>Country</th><th>Cases</th><th>Cases Today</th><th>Deaths</th><th>Deaths Today</th><th>Recovered</th><th>Active</th><th>Confirmed</th></tr></thead>" + 
+                            "<tbody><tr>" + 
+                                    "<td>" + marker.country + "</td>" + 
+                                    "<td>" + marker.cases + "</td>" + 
+                                    "<td>" + marker.todayCases + "</td>" + 
+                                    "<td>" + marker.deaths + "</td>" + 
+                                    "<td>" + marker.todayDeaths + "</td>" + 
+                                    "<td>" + marker.recovered + "</td>" + 
+                                    "<td>" + marker.active + "</td>" + 
+                                    "<td>" + marker.confirmed + "</td>" + 
+                            "</tr></tbody></table></div>"
 
-                        let flagIcon = L.icon({
-                            iconUrl: marker.countryInfo.flag,
-                            iconSize: [25, 15]
-                        });
+                let flagIcon = L.icon({
+                    iconUrl: marker.countryInfo.flag,
+                    iconSize: [25, 15]
+                });
 
-                        var noHide = false;
-                        L.marker([marker.countryInfo.lat, marker.countryInfo.long], {
-                            icon: flagIcon
-                        }).bindPopup(popupContent)
-                          .bindTooltip(marker.countryInfo.iso3, {
-                                direction: 'auto'
-                          })
-                          .openPopup()
-                          .addTo(map)
-                          .on('click', function () {
-                                this.setLabelNoHide(noHide);
-                                noHide = !noHide;
-                           });
-                });    
-            }
+                var noHide = false;
+                L.marker([marker.countryInfo.lat, marker.countryInfo.long], {
+                    icon: flagIcon
+                }).bindPopup(popupContent)
+                    .bindTooltip(marker.countryInfo.iso3, {
+                        direction: 'auto'
+                    })
+                    .openPopup()
+                    .addTo(countries)
+                    .on('click', function () {
+                        this.setLabelNoHide(noHide);
+                        noHide = !noHide;
+                    });
+                
+                let heatPoints = [marker.countryInfo.lat, marker.countryInfo.long, marker.cases];
+                heatRootPoints.push(heatPoints);
+            });
+
+            let heats = 
+                L.heatLayer(heatRootPoints, {
+                    radius: 35,
+                    blur: 35
+                }).addTo(map);    
+            
+            countries.addTo(map);            
+            overLayers['Heatmap Cases'] = heats;
+            overLayers['Countries Coverage'] = countries;
+        }
     });
 
-    L.control.layers(baseLayers, null).addTo(map);
+    L.control.layers(baseLayers, overLayers, { collapsed: false }).addTo(map);
 
     L.control.scale().addTo(map);
 
